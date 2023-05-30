379de7503bc59437e15c8c18a9cf3883cd1f3ca3
xmentosx <xmentosx@gmail.com>
2023-05-30 18:40:12 +0300

    В классе ArrayStorage реализованы методы: save, get, delete, clear, getAll, size.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,61 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size = 0;
 
     void clear() {
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
+        size = 0;
     }
 
-    void save(Resume r) {
+    void save(Resume resume) {
+        if (storage.length == size) {
+            System.out.println("Невозможно добавить резюме: Хранилище полностью заполнено.");
+            return;
+        }
+        storage[size++] = resume;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+        }
+        System.out.println("Невозможно получить резюме: Резюме не найдено в хранилище.");
         return null;
     }
 
     void delete(String uuid) {
+        boolean resumeFound = false;
+        for (int i = 0; i < size; i++) {
+            if (resumeFound) {
+                storage[i - 1] = storage[i];
+            } else if (storage[i].uuid.equals(uuid)) {
+                resumeFound = true;
+            }
+        }
+        if (resumeFound) {
+            storage[size--] = null;
+        } else {
+            System.out.println("Невозможно удалить резюме: Резюме не найдено в хранилище.");
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
