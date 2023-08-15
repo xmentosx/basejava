8b6fe1021003c2473f467b403ad24ae2d41a7e26
xmentosx <xmentosx@gmail.com>
2023-08-16 00:41:55 +0300

    HW4: 1) implemented AbstractArrayStorageTest, ArrayStorageTest и SortedArrayStorageTest 2) used invoke for toString in MainReflection

--- src/com/urise/webapp/MainReflection.java
+++ src/com/urise/webapp/MainReflection.java
@@ -3,17 +3,18 @@ package com.urise.webapp;
 import com.urise.webapp.model.Resume;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 
 public class MainReflection {
 
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         Resume r = new Resume();
         Field field = r.getClass().getDeclaredFields()[0];
         field.setAccessible(true);
         System.out.println(field.getName());
         System.out.println(field.get(r));
         field.set(r, "new_uuid");
-        // TODO : invoke r.toString via reflection
         System.out.println(r);
+        System.out.println(r.getClass().getMethod("toString").invoke(r));
     }
 }
--- src/com/urise/webapp/storage/AbstractArrayStorageTest.java
+++ src/com/urise/webapp/storage/AbstractArrayStorageTest.java
@@ -1,9 +1,12 @@
 package com.urise.webapp.storage;
 
+import com.urise.webapp.exception.ExistStorageException;
+import com.urise.webapp.exception.NotExistStorageException;
+import com.urise.webapp.exception.StorageException;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import com.urise.webapp.exception.NotExistStorageException;
+
 import com.urise.webapp.model.Resume;
 
 public abstract class AbstractArrayStorageTest {
@@ -12,6 +15,10 @@ public abstract class AbstractArrayStorageTest {
     private static final String UUID_1 = "uuid1";
     private static final String UUID_2 = "uuid2";
     private static final String UUID_3 = "uuid3";
+
+    public AbstractArrayStorageTest(Storage storage) {
+        this.storage = storage;
+    }
 
     @Before
     public void setUp() throws Exception {
@@ -28,32 +35,60 @@ public abstract class AbstractArrayStorageTest {
 
     @Test
     public void clear() throws Exception {
-
+        storage.clear();
+        Assert.assertEquals(0, storage.size());
     }
 
     @Test
     public void update() throws Exception {
-
+        storage.update(new Resume(UUID_1));
+        Assert.assertEquals(3, storage.size());
     }
 
     @Test
     public void getAll() throws Exception {
-
+        Resume[] resumes = storage.getAll();
+        Assert.assertEquals(3, resumes.length);
     }
 
     @Test
     public void save() throws Exception {
+        storage.save(new Resume("uuid4"));
+        Assert.assertEquals(4, storage.size());
+    }
 
+    @Test(expected = StorageException.class)
+    public void saveOverflow() throws Exception {
+        try {
+            for (int i = 4; i <= 10000; i++) {
+                storage.save(new Resume("uuid" + i));
+            }
+        } catch (StorageException e) {
+            Assert.fail("Early storage overflow");
+        }
+        storage.save(new Resume("uuid10001"));
+    }
+
+    @Test(expected = ExistStorageException.class)
+    public void saveExist() throws Exception {
+        storage.save(new Resume(UUID_1));
     }
 
     @Test
     public void delete() throws Exception {
+        storage.delete(UUID_1);
+        Assert.assertEquals(2, storage.size());
+    }
 
+    @Test(expected = NotExistStorageException.class)
+    public void deleteNotExist() throws Exception {
+        storage.delete("dummy");
     }
 
     @Test
     public void get() throws Exception {
-
+        Resume resume = storage.get(UUID_2);
+        Assert.assertEquals(new Resume(UUID_2), resume);
     }
 
     @Test(expected = NotExistStorageException.class)
